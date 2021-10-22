1891f9dc5bf5ba209752933104ad32647334a256
Murilo <murilocarrijoadm@gmail.com>
2021-10-22 19:09:57 -0300

    Rquisitos 1 e 3 refatorados

--- src/page/ProfileEdit.js
+++ src/page/ProfileEdit.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../Components/Header';
 
 class ProfileEdit extends React.Component {
   render() {
     return (
       <section>
         <div data-testid="page-profile-edit">
+          <Header />
           <h1>Editar Perfil</h1>
         </div>
       </section>
